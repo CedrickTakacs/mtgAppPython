399baf07366c282843c630bb0b646cfda4a149ce
Cedrick Takacs <cedtakacs@outlook.com>
2023-06-20 23:00:44 -0400

    it work

--- app.py
+++ app.py
@@ -3,10 +3,30 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-def search_cards(query):
-    url = "https://api.scryfall.com/cards/search"
-    params = {"q": query}
+def get_cad_price(usd_price):
+    api_key = '6eb31aae1bcc4226aff9be25733ec63f'
+    url = f"https://openexchangerates.org/api/latest.json?app_id={api_key}&base=USD&symbols=CAD"
 
+    response = requests.get(url)
+    data = response.json()
+
+    if response.status_code == 200:
+        rates = data.get("rates", {})
+        cad_rate = rates.get("CAD")
+        if cad_rate:
+            cad_price = float(usd_price) * cad_rate
+            return round(cad_price, 2)
+    return None
+
+def search_cards(query, search_type):
+    if search_type == "name":
+        params = {"q": query}
+    elif search_type == "number":
+        params = {"order": "number", "q": query}
+    else:
+        return None
+
+    url = "https://api.scryfall.com/cards/search"
     response = requests.get(url, params=params)
     data = response.json()
 
@@ -18,28 +38,41 @@ def search_cards(query):
             card_type = result.get("type_line")
             card_set = result.get("set_name")
             card_rarity = result.get("rarity")
+            card_image = result.get("image_uris", {}).get("normal")
             prices = result.get("prices", {})
             usd_price = prices.get("usd")
-            image_uris = result.get("image_uris", {})
-            card_image = image_uris.get("normal")
 
             if usd_price:
-                cards.append({
-                    "name": card_name,
-                    "type": card_type,
-                    "set": card_set,
-                    "rarity": card_rarity,
-                    "usd_price": usd_price,
-                    "image": card_image
-                })
+                cad_price = get_cad_price(usd_price)
+                if cad_price:
+                    cards.append({
+                        "name": card_name,
+                        "type": card_type,
+                        "set": card_set,
+                        "rarity": card_rarity,
+                        "image": card_image,
+                        "usd_price": usd_price,
+                        "cad_price": cad_price
+                    })
+                else:
+                    cards.append({
+                        "name": card_name,
+                        "type": card_type,
+                        "set": card_set,
+                        "rarity": card_rarity,
+                        "image": card_image,
+                        "usd_price": usd_price,
+                        "cad_price": None
+                    })
             else:
                 cards.append({
                     "name": card_name,
                     "type": card_type,
                     "set": card_set,
                     "rarity": card_rarity,
+                    "image": card_image,
                     "usd_price": None,
-                    "image": card_image
+                    "cad_price": None
                 })
         return cards
     else:
@@ -49,10 +82,13 @@ def search_cards(query):
 def index():
     if request.method == 'POST':
         search_query = request.form.get('search_query', '')
-        cards = search_cards(search_query)
+        search_type = request.form.get('search_type', 'name')
+        cards = search_cards(search_query, search_type)
         return render_template('index.html', cards=cards)
     else:
         return render_template('index.html', cards=None)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='192.168.2.162')
+    # app.run(debug=True)
+
--- static/script.js
+++ static/script.js
@@ -1,18 +1,25 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var searchForm = document.getElementById("search-form");
-    searchForm.addEventListener("submit", function(event) {
-        event.preventDefault();
-        var searchQuery = document.getElementById("search-query").value;
-        fetch("/", {
-            method: "POST",
-            body: JSON.stringify({ search_query: searchQuery }),
-            headers: {
-                "Content-Type": "application/json",
-            }
-        }).then(function(response) {
-            return response.text();
-        }).then(function(html) {
-            document.getElementById("card-container").innerHTML = html;
-        });
+document.addEventListener('DOMContentLoaded', function () {
+    const searchForm = document.getElementById('search-form');
+    searchForm.addEventListener('submit', function (event) {
+        event.preventDefault(); // Prevent form submission
+
+        const searchQuery = document.getElementById('search-query').value.trim();
+        if (searchQuery !== '') {
+            fetch('/',
+                {
+                    method: 'POST',
+                    body: new URLSearchParams({
+                        'search_query': searchQuery
+                    })
+                })
+                .then(response => response.text())
+                .then(data => {
+                    const cardContainer = document.getElementById('card-container');
+                    cardContainer.innerHTML = data;
+                    searchForm.reset(); // Reset the form input field
+                })
+                .catch(error => console.error('Error:', error));
+        }
     });
 });
+
